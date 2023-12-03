b64cd09a98dc88212283970f03f9914a8e930f4d
alexander-slavchev <a.slavchev19.48@gmail.com>
2023-12-03 22:01:49 +0200

    showing table with angular

--- API/Program.cs
+++ API/Program.cs
@@ -15,11 +15,12 @@ internal class Program
         {
             opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
+        builder.Services.AddCors();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-     
+        app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
         app.UseHttpsRedirection();
 
